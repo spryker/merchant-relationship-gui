5b49d809693cab88957e003b259eadc6ea21a36c
Dmitry Lymarenko <dmitry.lymarenko@spryker.com>
2019-09-19 17:19:45 +0300

    PS-10664 Recatored code according to convintions

--- src/Spryker/Zed/MerchantRelationshipGui/Communication/Plugin/ProductListGui/MerchantRelationListProductListTopButtonsExpanderPlugin.php
+++ src/Spryker/Zed/MerchantRelationshipGui/Communication/Plugin/ProductListGui/MerchantRelationListProductListTopButtonsExpanderPlugin.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\MerchantRelationshipGui\Communication\Plugin\ProductListGuiExtension;
+namespace Spryker\Zed\MerchantRelationshipGui\Communication\Plugin\ProductListGui;
 
 use Generated\Shared\Transfer\ButtonCollectionTransfer;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
